d969580d331eb9d9d2c3ccc3e47c828a76c9b89b
Arianna Bianchi <bianchi.aribi@gmail.com>
2023-11-28 23:01:45 +0100

    submission

--- preprocess_data.py
+++ preprocess_data.py
@@ -52,56 +52,38 @@ def user_description_sentiment_analysis(s):
         return ''
 
 
-# preprocessing for similar items
-# A lot of the descriptions (and other features) contain HTML.
-# The function parses and "translates" into plain text descriptions more suitable for analysis.
-def process_and_stemming(s):
-    stop_words = set(stopwords.words('english'))
-    stemmer= PorterStemmer()
-    if not s or s.isspace(): 
-        # print("Empty description", s, "empty")
-        return ''
-    try:
-        # remove html tags 
-        strr = str(html.fromstring(s).text_content())
-        # remove URLs
-        strr = re.sub(r"(https|http|href)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b", ' ', strr)
-        # remove html hidden carachters 
-        strr = strr.replace('\n', ' ').replace('\t', ' ').replace("&nbsp", ' ').replace('\r', ' ')
-        # remove punctuation
-        strr = re.sub(r'[^\w\s]|[_+]', ' ', strr)
-        # remove numbers
-        strr = re.sub(r'\d+', '', strr)
-        # lowercase
-        strr = strr.lower()
-        # remove stop words
-        tokens = nltk.word_tokenize(strr)
-        strr = [i for i in tokens if not i in stop_words]
-        # stemming
-        strr = [stemmer.stem(word) for word in strr]
-        strr = ' '.join(strr)
-        return strr 
-    except etree.ParserError: 
-        return ''
-    
 # # preprocessing for similar items
 # # A lot of the descriptions (and other features) contain HTML.
 # # The function parses and "translates" into plain text descriptions more suitable for analysis.
-# def stemming_data(s):
-#     # stop_words = set(stopwords.words('english'))
+# def process_and_stemming(s):
+#     stop_words = set(stopwords.words('english'))
 #     stemmer= PorterStemmer()
 #     if not s or s.isspace(): 
 #         # print("Empty description", s, "empty")
 #         return ''
 #     try:
+#         # remove html tags 
+#         strr = str(html.fromstring(s).text_content())
+#         # remove URLs
+#         strr = re.sub(r"(https|http|href)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b", ' ', strr)
+#         # remove html hidden carachters 
+#         strr = strr.replace('\n', ' ').replace('\t', ' ').replace("&nbsp", ' ').replace('\r', ' ')
+#         # remove punctuation
+#         strr = re.sub(r'[^\w\s]|[_+]', ' ', strr)
+#         # remove numbers
+#         strr = re.sub(r'\d+', '', strr)
+#         # lowercase
+#         strr = strr.lower()
+#         # remove stop words
+#         tokens = nltk.word_tokenize(strr)
+#         strr = [i for i in tokens if not i in stop_words]
 #         # stemming
-#         strr = s
 #         strr = [stemmer.stem(word) for word in strr]
 #         strr = ' '.join(strr)
 #         return strr 
 #     except etree.ParserError: 
 #         return ''
-    
+
     
 
 # preprocessing only for removing html, urls and hidden characters
